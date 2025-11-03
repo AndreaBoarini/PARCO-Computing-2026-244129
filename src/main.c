d800b44aa8dc22e50833a72a4299497819d55516
Andrea Boarini <andreaboa26@gmail.com>
2025-11-03 17:58:38 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -13,10 +13,9 @@ int main(int argc, char *argv[]) {
     int *I, *J, i, *row_ptr = NULL;
     double *val;
 
-
     srand(time(NULL));
 
-    // read mtx file from matrix market
+    // read mtx file from matrix market format
     f = fopen(argv[1], "r");
     if(f == NULL){
         printf("Error opening file\n");
@@ -35,9 +34,9 @@ int main(int argc, char *argv[]) {
 
     fclose(f);
 
-    printMatrixInCoo(I, J, val, nz);
+    // printMatrixInCoo(I, J, val, nz);
     row_ptr = COOtoCSR(I, J, val, nz, M);
-    printMatrixInCSR(row_ptr, J, val, nz, M);
+    // printMatrixInCSR(row_ptr, J, val, nz, M);
 
     // generate a random vector of size N (columns of the matrix)
     double max = 4.0, min = -4.0, range, div;
@@ -48,28 +47,22 @@ int main(int argc, char *argv[]) {
         vec[i] = min + (rand() / div);
     }
 
-    for(i = 0; i < N; i++) {
-        printf("%0.2g ", vec[i]);
-    }
+    printVectorDouble(vec, N, "random_vector");
 
     // compute the matrix-vector product
-    double start_prod, finish_prod;
+    double start_time, finish_time;
     double *result = calloc(M, sizeof(double));
-    GET_TIME(start_prod);
+    GET_TIME(start_time);
     for(int i = 0; i < M; i++) {
         for(int j = row_ptr[i]; j < row_ptr[i+1]; j++) {
             result[i] += val[j] * vec[J[j]-1]; // -1 for 0-based indexing
         }
     }
-    GET_TIME(finish_prod);
-    double elapsed_prod = finish_prod - start_prod;
-    printf("\nElapsed time for matrix-vector product: %f\n", elapsed_prod);
+    GET_TIME(finish_time);
+    double elapsed_time = finish_time - start_time;
+    printf("Elapsed time for matrix-vector product: %f\n", elapsed_time);
 
-    printf("\nResult:\n");
-    for(i = 0; i < M; i++) {
-        printf("%0.2g ", result[i]);
-    }
-    printf("\n");
+    printVectorDouble(result, M, "result_vector");
 
     free(I);
     free(J);
