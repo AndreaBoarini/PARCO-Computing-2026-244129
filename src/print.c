6bdad169ec7d08115b59c27c4acc22ae5015808a
Andrea Boarini <andreaboa26@gmail.com>
2025-11-03 17:57:59 +0100

    improved print for 'int' and 'double' vector types

--- src/print.c
+++ src/print.c
@@ -2,11 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void printVector(int *vec, int size, char* tag){
+void printVectorInt(int *vec, int size, char* tag){
     int i;
     printf("%s = [", tag);
     for(i = 0; i < size; i++) {
         printf(" %d ", vec[i]);
+    }
+    printf("]\n");
+}
+
+void printVectorDouble(double *vec, int size, char* tag){
+    int i;
+    printf("%s = [", tag);
+    for(i = 0; i < size; i++) {
+        printf(" %0.2f ", vec[i]);
     }
     printf("]\n");
 }
@@ -20,25 +29,15 @@ void printMatrixIntrinsic(int *I, int *J, double *val, int nz) {
 }
 
 void printMatrixInCoo(int *I, int *J, double *val, int nz) {
-    printVector(I, nz, "row_ind");
-    printVector(J, nz, "col_ind");
-    int i;
-    printf("%s = [", "val");
-    for(i = 0; i < nz; i++) {
-        printf(" %g ", val[i]);
-    }
-    printf("]\n");
+    printVectorInt(I, nz, "row_ind");
+    printVectorInt(J, nz, "col_ind");
+    printVectorDouble(val, nz, "val");
     printf("non-zero elements: %d\n", nz);
 }
 
 void printMatrixInCSR(int *row_ptr, int *J, double *val, int nz, int n_rows) {
-    printVector(row_ptr, n_rows + 1, "row_ptr");
-    printVector(J, nz, "col_ind");
-    int i;
-    printf("%s = [", "val");
-    for(i = 0; i < nz; i++) {
-        printf(" %g ", val[i]);
-    }
-    printf("]\n");
+    printVectorInt(row_ptr, n_rows + 1, "row_ptr");
+    printVectorInt(J, nz, "col_ind");
+    printVectorDouble(val, nz, "val");
     printf("non-zero elements: %d\n", nz);
 }
