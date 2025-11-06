fc127ea3961579f8c98527903d6a837cb788a739
Andrea Boarini <andreaboa26@gmail.com>
2025-11-06 21:24:13 +0100

    added terminal input support + OMP flag verification

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <string.h>
 #include "timer.h"
 #include "mmio.h"
 #include "print.h"
@@ -8,12 +9,48 @@
 
 int main(int argc, char *argv[]) {
 
-    // define the environment (thread infos, scheduling and chunksize)
-    int num_of_threads = 4;
-    int chunk_size = 1;
-    omp_sched_t schedule_type = omp_sched_static;
-    omp_set_num_threads(num_of_threads);
-    omp_set_schedule(schedule_type, chunk_size);
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s <mtx_file> \n", argv[0]);
+        exit(EXIT_FAILURE);
+    } else {
+        // check if omp compiler flag is enabled
+        #ifdef _OPENMP
+        // define the environment (thread infos, scheduling and chunksize)
+        // initialize with default values
+        long num_of_threads = 1;
+        long chunk_size = 1;
+        omp_sched_t schedule_type = omp_sched_static;
+        if(argc < 5 || argc > 5) {
+            fprintf(stderr, "Usage: %s <mtx_file> <num_of_threads> <schedule_type> <chunk_size>\n", argv[0]);
+            exit(EXIT_FAILURE);
+        } else {
+            num_of_threads = strtol(argv[2], NULL, 10);
+            chunk_size = strtol(argv[4], NULL, 10);
+            printf("num_of_threads: %ld, chunk_size: %ld\n", num_of_threads, chunk_size);
+            if (strcmp(argv[3], "static") == 0) {
+                schedule_type = omp_sched_static;
+                printf("schedule_type: %d\n", schedule_type);
+            } else if (strcmp(argv[3], "dynamic") == 0) {
+                schedule_type = omp_sched_dynamic;
+                printf("schedule_type: %d\n", schedule_type);
+            } else if (strcmp(argv[3], "guided") == 0) {
+                schedule_type = omp_sched_guided;
+                printf("schedule_type: %d\n", schedule_type);
+            } else {
+                fprintf(stderr, "Unknown schedule type. Use 'static', 'dynamic', or 'guided'.\n");
+                exit(EXIT_FAILURE);
+            }
+            omp_set_num_threads(num_of_threads);
+            omp_set_schedule(schedule_type, chunk_size);
+        }
+        #else
+        if(argc != 2) {
+            fprintf(stderr, "Too many arguments. Usage: %s <mtx_file>\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        #endif
+        
+    }
     
     srand(time(NULL));
     MM_typecode matcode;
