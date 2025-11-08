21a360f8f01ffcb682f69dc1f5db904f0f12d36e
Andrea Boarini <andreaboa26@gmail.com>
2025-11-08 21:05:51 +0100

    test file for perf

---
+++ HelloCache.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    
+    int a, b = 3;
+    int* vec = calloc(10, sizeof(int));
+    for (a = 0; a < 10; a++) {
+        vec[a] = a;
+    }
+
+    for (a = 0; a < 10; a++) {
+        vec[a] += b;
+    }
+
+    return 0;
+}
