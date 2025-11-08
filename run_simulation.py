493f0da7011f791dd738c68ff592b926e549cc68
Andrea Boarini <andreaboa26@gmail.com>
2025-11-08 10:23:09 +0100

    sequential simulation

--- run_simulation.py
+++ run_simulation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import subprocess
 
 data_dir_path = "./data"
+time_simulation_results = "times.csv"
 
 # extract from each file the matrices' intrinsics
 input_matrices = []
@@ -27,16 +28,22 @@ chunk_sizes_options = {1, 10, 100, 1000}
 scheduling_options = {"static", "dynamic", "guided"}
 src_files = {"src/main.c", "src/csr.c", "src/print.c", "src/mmio.c"}
 
+# create the csv for time results
+with open(time_simulation_results, mode='w', newline='') as f:
+    writer = csv.writer(f)
+    header = ["matrix_name", "rows", "cols", "nz", "compiler_option", "thread_option", "chunk_size_option", "scheduling_option", "exec_time"]
+    writer.writerow(header)
+
 # run the sequential simulation with the matrices available
 # in ./data and exploiting -O0, ... -Ofast optimization levels
 print("starting sequential simulation...")
 for co in compiler_options:
-    print(f"Compiling with option: {co}...")
     subprocess.run(["gcc", "-g", "-Iinclude", "-o", "main", *src_files, co, "-o", "main"])
     for matrix in input_matrices:
         matrix_file, M, N, nz = matrix
-        result = subprocess.run(["./main", os.path.join(data_dir_path, matrix_file)],
-                                capture_output=True, text=True)
-        with open("sequential_results.csv", "a", newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow([matrix_file, M, N, nz, co, result.stdout.strip()])
+        for i in range(1, 11):
+            with open(time_simulation_results, mode='a', newline='') as f:
+                writer = csv.writer(f)
+                result = subprocess.run(["./main", (data_dir_path + "/" + matrix_file)], capture_output=True, text=True)
+                exec_time = result.stdout.strip()
+                writer.writerow([matrix_file, M, N, nz, co, 'Nan', 'Nan', 'Nan', exec_time])
