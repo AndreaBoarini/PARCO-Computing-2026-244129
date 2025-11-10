73f9280f0ac0fa3b0cf03aa1828f51f6b40aa83b
Andrea Boarini <andreaboa26@gmail.com>
2025-11-10 21:50:14 +0100

    fixed poor sorting performances. Swithced to O(nlog(n)) algorithm

--- src/csr.c
+++ src/csr.c
@@ -1,5 +1,24 @@
+#include <stdlib.h>
 #include "csr.h"
 #include "mmio.h"
+
+typedef struct {
+    int row;
+    int col;
+    double value;
+} coo_instance;
+
+// define the sorting logic
+int compare_coo_instances(const void *a, const void *b) {
+    coo_instance *inst_a = (coo_instance *)a;
+    coo_instance *inst_b = (coo_instance *)b;
+
+    if (inst_a->row != inst_b->row) {
+        return inst_a->row - inst_b->row;
+    } else {
+        return inst_a->col - inst_b->col;
+    }
+}
 
 void readMtx(char* filename, int **I, int **J, double **val, int *nz, int *M, int *N) {
     FILE *f;
@@ -30,23 +49,28 @@ int* COOtoCSR(int *I, int *J, double *val, int nz, int n_rows) {
     int i, j, temp_vec;
     double temp_val;
     int* output_row_ptr = calloc((n_rows + 1), sizeof(int));
-
-    // prepare the COO arrays by sorting them
-    for(i = 0; i < nz; i++) {
-        for(j = i + 1; j < nz; j++) {
-            if(I[i] > I[j] || (I[i] == I[j] && J[i] > J[j])) {
-                temp_vec = I[i];
-                I[i] = I[j];
-                I[j] = temp_vec;
-                temp_vec = J[i];
-                J[i] = J[j];
-                J[j] = temp_vec;
-                temp_val = val[i];
-                val[i] = val[j];
-                val[j] = temp_val;
-            }
-        }
+    coo_instance* coo_array = malloc(nz * sizeof(coo_instance));
+    if(coo_array == NULL || output_row_ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
     }
+
+    // switch to coo_instance structure
+    for(i = 0; i < nz; i++) {
+        coo_array[i].row = I[i];
+        coo_array[i].col = J[i];
+        coo_array[i].value = val[i];
+    }
+
+    qsort(coo_array, nz, sizeof(coo_instance), compare_coo_instances);
+
+    // reconstrucrt the original arrays once sorted
+    for(i = 0; i < nz; i++) {
+        I[i] = coo_array[i].row;
+        J[i] = coo_array[i].col;
+        val[i] = coo_array[i].value;
+    }
+    free(coo_array);
 
     // execute the conversion
     // count the number of elements in each row (result shifted by 1)
