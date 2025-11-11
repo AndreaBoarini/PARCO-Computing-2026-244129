abff80d0bd0d61a6dfe413197dbd9971ba6534d3
Andrea Boarini <andreaboa26@gmail.com>
2025-11-11 17:12:25 +0100

    test perf info extraction

--- script_bash.sh
+++ script_bash.sh
@@ -31,7 +31,8 @@ echo "matrix_name,rows,cols,nz,compiler_option,thread_option,chunk_size_option,s
 : '
 
 gcc -g -Iinclude "${src_files[@]}" -o main
-perf stat -e L1-dcache-loads,L1-dcache-load-misses,LLC-loads,LLC-misses ./main data/bcsstm21.mtx 2>&1
+perf stat -e L1-dcache-loads,L1-dcache-load-misses,LLC-loads,LLC-misses ./main data/bcsstm21.mtx 2>&1 \
+    | grep 'L1-dcache-loads'
 
 : '
 # Sequential simulation
