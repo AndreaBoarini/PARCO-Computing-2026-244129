ba613cbab6c6f202bb66e6e7ad71f9c89711630d
Andrea Boarini <andreaboa26@gmail.com>
2025-11-13 18:35:19 +0100

    added perf cold/warm start support

--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@
 
 int main(int argc, char *argv[]) {
 
+    bool perf_cold_start;
+
+    // argument inputs check
     if(argc < 2) {
         fprintf(stderr, "Usage: %s <mtx_file> \n", argv[0]);
         exit(EXIT_FAILURE);
@@ -22,20 +25,20 @@ int main(int argc, char *argv[]) {
         long num_of_threads = 1;
         long chunk_size = 1;
         omp_sched_t schedule_type = omp_sched_static;
-        if(argc < 5 || argc > 5) {
-            fprintf(stderr, "Usage: %s <mtx_file> <num_of_threads> <schedule_type> <chunk_size>\n", argv[0]);
+        if(argc < 6 || argc > 6) {
+            fprintf(stderr, "Usage: %s <perf_cold_start> | <mtx_file> <num_of_threads> <schedule_type> <chunk_size> \n", argv[0]);
             exit(EXIT_FAILURE);
         } else {
-            num_of_threads = strtol(argv[2], NULL, 10);
-            chunk_size = strtol(argv[4], NULL, 10);
+            num_of_threads = strtol(argv[3], NULL, 10);
+            chunk_size = strtol(argv[5], NULL, 10);
             //printf("num_of_threads: %ld, chunk_size: %ld\n", num_of_threads, chunk_size);
-            if (strcmp(argv[3], "static") == 0) {
+            if (strcmp(argv[4], "static") == 0) {
                 schedule_type = omp_sched_static;
                 //printf("schedule_type: %d\n", schedule_type);
-            } else if (strcmp(argv[3], "dynamic") == 0) {
+            } else if (strcmp(argv[4], "dynamic") == 0) {
                 schedule_type = omp_sched_dynamic;
                 //printf("schedule_type: %d\n", schedule_type);
-            } else if (strcmp(argv[3], "guided") == 0) {
+            } else if (strcmp(argv[4], "guided") == 0) {
                 schedule_type = omp_sched_guided;
                 //printf("schedule_type: %d\n", schedule_type);
             } else {
@@ -46,11 +49,21 @@ int main(int argc, char *argv[]) {
             omp_set_schedule(schedule_type, chunk_size);
         }
         #else
-        if(argc != 2) {
-            fprintf(stderr, "Too many arguments. Usage: %s <mtx_file>\n", argv[0]);
+        if(argc != 3) {
+            fprintf(stderr, "Too many/few arguments. Usage: %s <perf_cold_start> | <mtx_file>\n", argv[0]);
             exit(EXIT_FAILURE);
         }
         #endif
+
+        if(strcmp(argv[1], "C") == 0) {
+            perf_cold_start = true;
+        } else if (strcmp(argv[1], "W") == 0) {
+            perf_cold_start = false;
+        } else {
+            fprintf(stderr, "Unknown perf_cold_start value. Use 'C' or 'W'.\n");
+            fprintf(stderr, "Usage: %s <perf_cold_start> | <mtx_file> <num_of_threads> <schedule_type> <chunk_size> \n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
         
     }
     
@@ -61,11 +74,9 @@ int main(int argc, char *argv[]) {
     double *val;
 
     // read mtx file from matrix market format
-    readMtx(argv[1], &I, &J, &val, &nz, &M, &N);
+    readMtx(argv[2], &I, &J, &val, &nz, &M, &N);
 
-    // printMatrixInCoo(I, J, val, nz);
     row_ptr = COOtoCSR(I, J, val, nz, M);
-    // printMatrixInCSR(row_ptr, J, val, nz, M);
 
     // generate a random vector of size N (columns of the matrix)
     double max = 4.0, min = -4.0, range, div;
@@ -76,28 +87,31 @@ int main(int argc, char *argv[]) {
         vec[i] = min + (rand() / div);
     }
 
-    //printVectorDouble(vec, N, "random_vector");
-
     // compute the matrix-vector product
-    double start_time, finish_time;
-    int j;
+    double start_time, finish_time, elapsed_time;
+    int j, iterations, limit;
+    if(perf_cold_start)
+        limit = 1;
+    else
+        limit = 10;        
     double *result = calloc(M, sizeof(double));
-    GET_TIME(start_time);
+    for(iterations = 0; iterations < limit; iterations++) {
+        GET_TIME(start_time);
 
-    #pragma omp parallel for schedule (runtime)
-    for(i = 0; i < M; i++) {
-        for(j = row_ptr[i]; j < row_ptr[i+1]; j++) {
-            result[i] += val[j] * vec[J[j]-1]; // -1 for 0-based indexing
+        #pragma omp parallel for schedule(runtime)
+        for(i = 0; i < M; i++) {
+            double sum = 0.0;
+            for(j = row_ptr[i]; j < row_ptr[i+1]; j++) {
+                sum += val[j] * vec[J[j]-1]; // -1 for 0-based indexing
+            }
+            result[i] = sum;
         }
+
+        GET_TIME(finish_time);
+        elapsed_time = finish_time - start_time;
+        elapsed_time *= 1000;
+        printf("%f\n", elapsed_time);
     }
-
-    GET_TIME(finish_time);
-    double elapsed_time = finish_time - start_time;
-    elapsed_time *= 1000;
-    //printf("Elapsed time for matrix-vector product (ms): %f\n", elapsed_time);
-    printf("%f\n", elapsed_time);
-
-    //printVectorDouble(result, M, "result_vector");
 
     free(I);
     free(J);
